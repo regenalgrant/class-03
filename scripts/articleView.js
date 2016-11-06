e3276ba5a5c040aaf8d657c6a9883ccb79553abb
regenalgrant <regenal@mac.com>
2016-11-06 11:59:19 -0800

    fixing

--- scripts/articleView.js
+++ scripts/articleView.js
@@ -1,29 +1,81 @@
-(function(module){
-  var articleView = {};
+var portfolioView = [];
+var aboutMeView = [];
 
-  articleView.handleMainNav = function() {
-    $('.main-nav').on('click', '.tab', function() {
-      $('.tab-content').hide();
-      $('#' + $(this).data().content()).fadeIn();
-    });
-    $('.main-nav .tab:first').click();
-  };
-  articleView.setTeasers = function() {
-    $('#portfolio *:nth-of-type(n+2)').hide();
+function portfolioView(opts) {
+  for (key in opts) {
+    this[key] = opts[key];
+  }
+}
+function AboutMe(opts) {
+  for (key in opts) {
+    this[key] = opts[key];
+  }
+}
 
-    $('#portfolio').on('click', 'a.read-on', function(event) {
-      event.preventDefault();
-      $(this).parent().find('*').fadeIn();
-      $(this).hide();
-    });
-  };
-  articleView.renderIndexPage = function() {
-    Article.allArticles.forEach(function(article) {
-      $('#portfolio').append(article.toHtml('#portfolio-template'));
-    });
-    $('#numFacts').text(Article.numWordsAll());
-    articleView.handleMainNav();
-    articleView.setTeasers();
-  };
-  module.articleView = articleView;
+// Piece.prototype.toHtml = function(){
+//   this.daysAgo = parseInt((new Date() - new Date(this.published)) / 60 / 60 / 24 / 1000);
+//   this.publishStatus = this.published ? 'published about  ' + this.daysAgo + ' days ago' : '(draft)';
+//   var source = $('#portfolio-items-template').html();
+//   var templateRender = Handlebars.compile(source);
+//   return templateRender(this);
+// };
+// 
+// AboutMe.prototype.toHtml = function () {
+//   var source = $('#about-items-template').html();
+//   var templateRender = Handlebars.compile(source);
+//   return templateRender(this);
+// };
+// portfolioPieces.handleMainNav = function () {
+//   $('.header-menu').on('click', '.contentBtn', function() {
+//     $('.contentBtn-content').hide();
+//     var $idContent = $(this).data().content;
+//     console.log($idContent, 'id Content');
+//     $('#' + $idContent).show();
+//     $idContent = '';
+//   });
+//   $('.header-menu .contentBtn:first').click();
+// };
+//
+// portfolioPieces.setTeasers = function() {
+//
+//   $('.body-portfolio-item').hide();
+//
+//   $('.read-on').on('click', function(event){
+//     console.log('clicked: ', $(this).text());
+//     event.preventDefault();
+//     if($(this).html() === 'Show More →'){
+//       $(this).parent().find('*').show();
+//       $(this).text('Show Less');
+//       console.log('I should show more now');
+//     } else {
+//       $(this).text('Show More →');
+//       $(this).siblings('.body-portfolio-item').hide();
+//       console.log('I should show less now');
+//     };
+//   });
+// };
+//
+// portfolioItems.sort(function(currentObject, nextObject) {
+//   return (new Date(nextObject.published)) - (new Date(currentObject.published));
+// });
+//
+// portfolioItems.forEach(function(ele) {
+//   portfolioPieces.push(new Piece(ele));
+// });
+//
+// portfolioPieces.forEach(function(article) {
+//   $('#home-top-third').append(article.toHtml());
+// });
+//
+// aboutMeItems.forEach(function(ele) {
+//   aboutMePieces.push(new AboutMe(ele));
+// });
+//
+// aboutMePieces.forEach(function(article) {
+//   $('#home-middle-third').append(article.toHtml());
+// });
+//
+// portfolioPieces.handleMainNav();
+// portfolioPieces.setTeasers();
+
 })(window);
