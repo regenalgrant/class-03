9df09677dd1604a92234f2ef7bea57346d70d0c3
regenalgrant <regenal@mac.com>
2016-11-06 17:55:16 -0800

    fixing add

---
+++ scripts/aboutme.js
@@ -0,0 +1,10 @@
+var aboutMe = [
+url :'https://www.linkedin.com/in/regenal-grant-94640a24',
+img : ''
+alt : ''
+height : '60%'
+width : '60%'
+},
+body : <p id = "aboutme-p">
+</p> <article class="professional">
+]
--- scripts/articleView.js
+++ scripts/articleView.js
@@ -1,7 +1,7 @@
 var portfolioView = [];
 var aboutMeView = [];
 
-function portfolioView(opts) {
+function portfolioV(opts) {
   for (key in opts) {
     this[key] = opts[key];
   }
@@ -12,70 +12,70 @@ function AboutMe(opts) {
   }
 }
 
-// Piece.prototype.toHtml = function(){
-//   this.daysAgo = parseInt((new Date() - new Date(this.published)) / 60 / 60 / 24 / 1000);
-//   this.publishStatus = this.published ? 'published about  ' + this.daysAgo + ' days ago' : '(draft)';
-//   var source = $('#portfolio-items-template').html();
-//   var templateRender = Handlebars.compile(source);
-//   return templateRender(this);
-// };
-// 
-// AboutMe.prototype.toHtml = function () {
-//   var source = $('#about-items-template').html();
-//   var templateRender = Handlebars.compile(source);
-//   return templateRender(this);
-// };
-// portfolioPieces.handleMainNav = function () {
-//   $('.header-menu').on('click', '.contentBtn', function() {
-//     $('.contentBtn-content').hide();
-//     var $idContent = $(this).data().content;
-//     console.log($idContent, 'id Content');
-//     $('#' + $idContent).show();
-//     $idContent = '';
-//   });
-//   $('.header-menu .contentBtn:first').click();
-// };
-//
-// portfolioPieces.setTeasers = function() {
-//
-//   $('.body-portfolio-item').hide();
-//
-//   $('.read-on').on('click', function(event){
-//     console.log('clicked: ', $(this).text());
-//     event.preventDefault();
-//     if($(this).html() === 'Show More →'){
-//       $(this).parent().find('*').show();
-//       $(this).text('Show Less');
-//       console.log('I should show more now');
-//     } else {
-//       $(this).text('Show More →');
-//       $(this).siblings('.body-portfolio-item').hide();
-//       console.log('I should show less now');
-//     };
-//   });
-// };
-//
-// portfolioItems.sort(function(currentObject, nextObject) {
-//   return (new Date(nextObject.published)) - (new Date(currentObject.published));
-// });
-//
-// portfolioItems.forEach(function(ele) {
-//   portfolioPieces.push(new Piece(ele));
-// });
-//
-// portfolioPieces.forEach(function(article) {
-//   $('#home-top-third').append(article.toHtml());
-// });
-//
-// aboutMeItems.forEach(function(ele) {
-//   aboutMePieces.push(new AboutMe(ele));
-// });
-//
-// aboutMePieces.forEach(function(article) {
-//   $('#home-middle-third').append(article.toHtml());
-// });
-//
-// portfolioPieces.handleMainNav();
-// portfolioPieces.setTeasers();
+PortfolioV.prototype.toHtml = function(){
+  this.daysAgo = parseInt((new Date() - new Date(this.published)) / 60 / 60 / 24 / 1000);
+  this.publishStatus = this.published ? 'published about  ' + this.daysAgo + ' days ago' : '(draft)';
+  var source = $('#portfolio-items-template').html();
+  var templateRender = Handlebars.compile(source);
+  return templateRender(this);
+};
 
-})(window);
+AboutMe.prototype.toHtml = function () {
+  var source = $('#about-items-template').html();
+  var templateRender = Handlebars.compile(source);
+  return templateRender(this);
+};
+portfolioView.handleMainNav = function () {
+  $('.header-menu').on('click', '.contentBtn', function() {
+    $('.contentBtn-content').hide();
+    var $idContent = $(this).data().content;
+
+    $('#' + $idContent).show();
+    $idContent = '';
+  });
+  $('.header-menu .contentBtn:first').click();
+};
+
+portfolioView.setTeasers = function() {
+
+  $('.body-portfolio-text').hide();
+
+  $('.read-on').on('click', function(event){
+
+    event.preventDefault();
+    if($(this).html() === 'Read on →'){
+      $(this).parent().find('*').show();
+      $(this).text('Collapse');
+
+    } else {
+      $(this).text('Read on→');
+      $(this).siblings('.body-portfolio-item').hide();
+
+    };
+  });
+};
+
+portfolioItems.sort(function(currentObject, nextObject) {
+  return (new Date(nextObject.published)) - (new Date(currentObject.published));
+});
+
+portfolio.forEach(function(ele) {
+  portfolioView.push(new Piece(ele));
+});
+
+portfolioView.forEach(function(article) {
+  $('#home-top-third').append(article.toHtml());
+});
+
+aboutMeView.forEach(function(ele) {
+  aboutMeView.push(new AboutMe(ele));
+});
+
+aboutMeView.forEach(function(article) {
+  $('#home-middle-third').append(article.toHtml());
+});
+
+portfolioView.handleMainNav();
+portfolioView.setTeasers();
+
+ })(window);
