0d174974a09f3a7eef72df26e0bed561d157fab3
regenalgrant <regenal@mac.com>
2016-11-07 10:01:46 -0800

    fixingJson

--- scripts/app.js
+++ scripts/app.js
@@ -1,81 +1,49 @@
-var portfolioView = [];
-var aboutMeView = [];
+(function (module) {
 
-function portfolioV(opts) {
-  for (key in opts) {
-    this[key] = opts[key];
+  function Project(opts) {
+    for (key in opts) {
+      this[key] = opts[key];
+    }
   }
-}
-function AboutMe(opts) {
-  for (key in opts) {
-    this[key] = opts[key];
-  }
-}
 
-PortfolioV.prototype.toHtml = function(){
-  this.daysAgo = parseInt((new Date() - new Date(this.published)) / 60 / 60 / 24 / 1000);
-  this.publishStatus = this.published ? 'published about  ' + this.daysAgo + ' days ago' : '(draft)';
-  var source = $('#portfolio-temp').html();
-  var templateRender = Handlebars.compile(source);
-  return templateRender(this);
-};
+  Project.projects = [];
 
-AboutMe.prototype.toHtml = function () {
-  var source = $('#about-temp').html();
-  var templateRender = Handlebars.compile(source);
-  return templateRender(this);
-};
-portfolioView.handleMainNav = function () {
-  $('.header-menu').on('click', '.contentBtn', function() {
-    $('.contentBtn-content').hide();
-    var $idContent = $(this).data().content;
+  Project.prototype.toHtml = function(){
+    var source = $('#portfolio-temp').html();
+    var templateRender = Handlebars.compile(source);
+    return templateRender(this);
+  };
 
-    $('#' + $idContent).show();
-    $idContent = '';
+  Project.setTeasers = function() {
+
+    $('.body-portfolio-text').hide();
+
+    $('.read-on').on('click', function(event){
+
+      event.preventDefault();
+      if($(this).html() === 'Read on →'){
+        $(this).parent().find('*').show();
+        $(this).text('Collapse');
+
+      } else {
+        $(this).text('Read on→');
+        $(this).siblings('.body-portfolio-text').hide();
+      };
+    });
+  };
+  Project.load = function (data) {
+    data.forEach(function (object) {
+      Project.projects.push(new Project(object));
+    });
+  };
+
+  Project.projects.forEach(function(article) {
+  $('#projects').append(article.toHtml());
   });
-  $('.header-menu .contentBtn:first').click();
-};
-
-portfolioView.setTeasers = function() {
-
-  $('.body-portfolio-text').hide();
-
-  $('.read-on').on('click', function(event){
-
-    event.preventDefault();
-    if($(this).html() === 'Read on →'){
-      $(this).parent().find('*').show();
-      $(this).text('Collapse');
-
-    } else {
-      $(this).text('Read on→');
-      $(this).siblings('.body-portfolio-text').hide();
-
-    };
-  });
-};
-
-portfolioText.sort(function(currentObject, nextObject) {
-  return (new Date(nextObject.published)) - (new Date(currentObject.published));
-});
-
-portfolioText.forEach(function(ele) {
-  portfolioView.push(new Piece(ele));
-});
-
-portfolioView.forEach(function(article) {
-  $('#home-top-third').append(article.toHtml());
-});
-
-aboutMeText.forEach(function(ele) {
-  aboutMeView.push(new AboutMe(ele));
-});
-
-aboutMeView.forEach(function(article) {
-  $('#home-middle-third').append(article.toHtml());
-});
-
-portfolioView.handleMainNav();
-portfolioView.setTeasers();
-
- })(window);
+  Project.getJson = function () {
+    $.getJSON ('data/Articles.json', function (data) {
+      console.log(data);
+    });
+  };
+  module.Project = Project;
+})(window);
--- scripts/articleView.js
+++ scripts/articleView.js
@@ -1,1 +1,11 @@
+var projectV = {};
 
+projectV.handleMainNav = function () {
+  $('.header-menu').on('click', '.contentBtn', function() {
+    $('.contentBtn-content').hide();
+    var $idContent = $(this).data().content;
+    $('#' + $idContent).show();
+    $idContent = '';
+  });
+  $('.header-menu .contentBtn:first').click();
+};
